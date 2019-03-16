aa91e10c0adbe7f52d05b0cbe2272d65af3855dd
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-16 00:34:18 +0000

    Removed unneeded libraries

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -4,8 +4,6 @@ echo "  Dir: $PWD"
 sh install-package.sh NUnit 2.6.4
 sh install-package.sh NUnit.Runners 2.6.4
 sh install-package.sh Newtonsoft.Json 11.0.2
-sh install-package.sh ArduinoSerialControllerClient 1.0.9
 sh install-package.sh duinocom.core 1.0.6
-sh install-package.sh M2Mqtt 4.3.0.0
 
 echo "Finished getting library files."
