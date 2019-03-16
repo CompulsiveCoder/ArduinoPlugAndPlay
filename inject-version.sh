5c02a481b3f5a4a358a60a5524313a8bb3a01cc4
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-16 00:31:31 +0000

    Added missing files

--- inject-version.sh
+++ inject-version.sh
@@ -6,8 +6,10 @@ FULL_VERSION="$VERSION.$BUILD"
 echo "Injecting version into AssemblyInfo.cs file..."
 echo "Version: $FULL_VERSION"
 
-ASSEMBLY_INFO_FILE="src/ArduinoPlugAndPlay/Properties/AssemblyInfo.cs"
-
-sed -i -e "s/1.0.\*/$FULL_VERSION/g" $ASSEMBLY_INFO_FILE
+sed -i -e "s/1.0.\*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay/Properties/AssemblyInfo.cs"
+sed -i -e "s/1.0.\*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.ClientConsole/Properties/AssemblyInfo.cs"
+sed -i -e "s/1.0.\*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests/Properties/AssemblyInfo.cs"
+sed -i -e "s/1.0.\*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests.Integration/Properties/AssemblyInfo.cs"
+sed -i -e "s/1.0.\*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests.Unit/Properties/AssemblyInfo.cs"
 
 echo "Finished injecting version"
