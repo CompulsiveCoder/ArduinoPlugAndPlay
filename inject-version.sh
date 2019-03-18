9585ad39e10a0fd9052a9f99cac8e93b40b7cbf7
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-18 00:46:29 +0000

    Fixed inject version script

--- inject-version.sh
+++ inject-version.sh
@@ -1,15 +1,21 @@
-VERSION=$(cat version.txt)
-BUILD=$(cat buildnumber.txt)
+VERSION_ARGUMENT=$1
 
-FULL_VERSION="$VERSION.$BUILD"
+if [ ! $VERSION_ARGUMENT ]; then
+  VERSION=$(cat version.txt)
+  BUILD=$(cat buildnumber.txt)
+
+  FULL_VERSION="$VERSION.$BUILD"
+else
+  FULL_VERSION=$VERSION_ARGUMENT
+fi
 
 echo "Injecting version into AssemblyInfo.cs file..."
 echo "Version: $FULL_VERSION"
 
-sed -i -e "s/1.0.0.1/$FULL_VERSION/g" "src/ArduinoPlugAndPlay/Properties/AssemblyInfo.cs"
-sed -i -e "s/1.0.0.1/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.ClientConsole/Properties/AssemblyInfo.cs"
-sed -i -e "s/1.0.0.1/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests/Properties/AssemblyInfo.cs"
-sed -i -e "s/1.0.0.1/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests.Integration/Properties/AssemblyInfo.cs"
-sed -i -e "s/1.0.0.1/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests.Unit/Properties/AssemblyInfo.cs"
+sed -i -e "s/1\.0\.0\.[.0-9]*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay/Properties/AssemblyInfo.cs"
+sed -i -e "s/1\.0\.0\.[.0-9]*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.ClientConsole/Properties/AssemblyInfo.cs"
+sed -i -e "s/1\.0\.0\.[.0-9]*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests/Properties/AssemblyInfo.cs"
+sed -i -e "s/1\.0\.0\.[.0-9]*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests.Integration/Properties/AssemblyInfo.cs"
+sed -i -e "s/1\.0\.0\.[.0-9]*/$FULL_VERSION/g" "src/ArduinoPlugAndPlay.Tests.Unit/Properties/AssemblyInfo.cs"
 
 echo "Finished injecting version"
