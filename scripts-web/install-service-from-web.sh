2b618c5437d6130010dc883fd04f368ab288fdb5
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-23 23:22:57 +0000

    Fixed script

--- scripts-web/install-service-from-web.sh
+++ scripts-web/install-service-from-web.sh
@@ -16,13 +16,7 @@ echo "$BRANCH"
 #SERVICE_FILE_PATH=$1
 #SERVICE_FILE=$(basename -- "$SERVICE_FILE_PATH")
 
-SYSTEMCTL_FILE_NAME="systemctl.sh"
-SYSTEMCTL_FILE_URL="https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/$SYSTEMCTL_FILE_NAME"
-
-echo "Downloading systemctl file..."
-echo "URL: $SYSTEMCTL_FILE_URL"
-echo "File name: $SYSTEMCTL_FILE_NAME"
-wget $SYSTEMCTL_FILE_URL -O $SYSTEMCTL_FILE_NAME || (echo "Failed to download $SYSTEMCTL_FILE_NAME." && exit 1)
+# Get the service file
 
 SERVICE_FILE_NAME="arduino-plug-and-play.service"
 SERVICE_FILE_URL="https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/svc/$SERVICE_FILE_NAME"
@@ -31,6 +25,24 @@ echo "Downloading service file..."
 echo "URL: $SERVICE_FILE_URL"
 echo "File name: $SERVICE_FILE_NAME"
 wget $SERVICE_FILE_URL -O $SERVICE_FILE_NAME || (echo "Failed to download $SERVICE_FILE_NAME." && exit 1)
+
+# Get the systemctl.sh helper script
+SYSTEMCTL_FILE_NAME="systemctl.sh"
+SYSTEMCTL_FILE_URL="https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/$SYSTEMCTL_FILE_NAME"
+
+echo "Downloading systemctl file..."
+echo "URL: $SYSTEMCTL_FILE_URL"
+echo "File name: $SYSTEMCTL_FILE_NAME"
+wget $SYSTEMCTL_FILE_URL -O $SYSTEMCTL_FILE_NAME || (echo "Failed to download $SYSTEMCTL_FILE_NAME." && exit 1)
+
+# Get the install-service.sh helper script
+INSTALL_SERVICE_SCRIPT_NAME="install-service.sh"
+INSTALL_SERVICE_SCRIPT_URL="https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/$INSTALL_SERVICE_SCRIPT_NAME"
+
+echo "Downloading service file..."
+echo "URL: $INSTALL_SERVICE_SCRIPT_URL"
+echo "File name: $INSTALL_SERVICE_SCRIPT_NAME"
+wget $INSTALL_SERVICE_SCRIPT_URL -O $INSTALL_SERVICE_SCRIPT_NAME || (echo "Failed to download $INSTALL_SERVICE_SCRIPT_NAME." && exit 1)
 
 echo "Installing service"
 #echo "Path: $SERVICE_FILE_PATH"
