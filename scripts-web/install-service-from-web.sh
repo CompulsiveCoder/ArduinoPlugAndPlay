eb613e4909927551ccb0cb5ce519e3875663cae6
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-23 23:00:26 +0000

    Added script

---
+++ scripts-web/install-service-from-web.sh
@@ -0,0 +1,69 @@
+echo "Installing plug and play service..."
+
+echo "Current directory:"
+echo "  $PWD"
+
+BRANCH=$1
+
+EXAMPLE_COMMAND="Example:\n..sh [branch]"
+
+if [ ! $BRANCH ]; then
+  BRANCH="master"
+fi
+
+echo "$BRANCH"
+
+#SERVICE_FILE_PATH=$1
+#SERVICE_FILE=$(basename -- "$SERVICE_FILE_PATH")
+
+SERVICE_FILE_NAME="arduino-plug-and-play.service"
+
+echo "Downloading service file..."
+wget "https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/dev/svc/$SERVICE_FILE_NAME" -O $PACKAGE_FILE_EXT || (echo "Failed to download $SERVICE_FILE_NAME." && exit 1)
+
+#echo "Installing service"
+#echo "Path: $SERVICE_FILE_PATH"
+#echo "Name: $SERVICE_FILE"
+
+#SYSTEMCTL_SCRIPT="systemctl.sh"
+
+#MOCK_SYSTEMCTL_FLAG_FILE="is-mock-systemctl.txt"
+
+#IS_MOCK_SYSTEMCTL=0
+
+#if [ -f "$MOCK_SYSTEMCTL_FLAG_FILE" ]; then
+#  IS_MOCK_SYSTEMCTL=1
+#  echo "Is mock systemctl"
+#fi
+
+#SERVICES_DIR="/lib/systemd/system"
+
+#if [ $IS_MOCK_SYSTEMCTL = 1 ]; then
+#  SERVICES_DIR="mock/services"
+#fi
+
+#SUDO=""
+#if [ ! "$(id -u)" -eq 0 ]; then
+#    SUDO='sudo'
+#fi
+
+#mkdir -p $SERVICES_DIR
+
+#echo "Services directory:"
+#echo "  $SERVICES_DIR"
+#echo "Destination file:"
+#echo "  $SERVICES_DIR/$SERVICE_FILE"
+
+#if [ $IS_MOCK_SYSTEMCTL = 1 ]; then
+#  echo "Is mock systemctl. Installing to mock directory."
+#  cp $SERVICE_FILE_PATH $SERVICES_DIR/$SERVICE_FILE
+#else
+#  $SUDO cp -fv $SERVICE_FILE_PATH $SERVICES_DIR/$SERVICE_FILE && \
+#  $SUDO chmod 644 $SERVICES_DIR/$SERVICE_FILE && \
+#  sh $SYSTEMCTL_SCRIPT daemon-reload && \
+#  sh $SYSTEMCTL_SCRIPT enable $SERVICE_FILE && \
+#  sh $SYSTEMCTL_SCRIPT start $SERVICE_FILE && \
+#  sh $SYSTEMCTL_SCRIPT restart $SERVICE_FILE
+#fi
+
+echo "Finished installing service"
