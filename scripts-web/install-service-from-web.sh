2f56e6aa12ae5edbaf7fbd2a3fb6c9493991d404
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-23 23:20:13 +0000

    Fixed script

--- scripts-web/install-service-from-web.sh
+++ scripts-web/install-service-from-web.sh
@@ -16,8 +16,15 @@ echo "$BRANCH"
 #SERVICE_FILE_PATH=$1
 #SERVICE_FILE=$(basename -- "$SERVICE_FILE_PATH")
 
-SERVICE_FILE_NAME="arduino-plug-and-play.service"
+SYSTEMCTL_FILE_NAME="systemctl.sh"
+SYSTEMCTL_FILE_URL="https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/$SYSTEMCTL_FILE_NAME"
 
+echo "Downloading systemctl file..."
+echo "URL: $SYSTEMCTL_FILE_URL"
+echo "File name: $SYSTEMCTL_FILE_NAME"
+wget $SYSTEMCTL_FILE_URL -O $SYSTEMCTL_FILE_NAME || (echo "Failed to download $SYSTEMCTL_FILE_NAME." && exit 1)
+
+SERVICE_FILE_NAME="arduino-plug-and-play.service"
 SERVICE_FILE_URL="https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/svc/$SERVICE_FILE_NAME"
 
 echo "Downloading service file..."
@@ -25,7 +32,7 @@ echo "URL: $SERVICE_FILE_URL"
 echo "File name: $SERVICE_FILE_NAME"
 wget $SERVICE_FILE_URL -O $SERVICE_FILE_NAME || (echo "Failed to download $SERVICE_FILE_NAME." && exit 1)
 
-#echo "Installing service"
+echo "Installing service"
 #echo "Path: $SERVICE_FILE_PATH"
 #echo "Name: $SERVICE_FILE"
 
