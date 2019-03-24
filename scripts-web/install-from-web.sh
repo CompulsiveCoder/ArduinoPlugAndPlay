53f31ad453b9b99c035df5366c7e47da41cd32c4
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-24 01:29:02 +0000

    Fixed scripts

--- scripts-web/install-from-web.sh
+++ scripts-web/install-from-web.sh
@@ -26,7 +26,7 @@ INSTALL_DIR=$DESTINATION
 #  INSTALL_DIR="mock/ArduinoPlugAndPlay"
 #fi
 
-#echo "Branch name: $BRANCH"
+echo "Branch name: $BRANCH"
 
 echo "Making install dir..."
 mkdir -p $INSTALL_DIR || exit 1
@@ -36,10 +36,10 @@ cd $INSTALL_DIR
 
 echo "Initializing plug and play (by downloading init-from-web.sh file)..."
 
-wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/init-from-web.sh | bash -s $BRANCH || (echo "Failed to initialize plug and play. Script: init-from-web.sh" && exit 1)
+wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/init-from-web.sh | bash -s - $BRANCH || (echo "Failed to initialize plug and play. Script: init-from-web.sh" && exit 1)
 
 echo "Initializing plug and play (by downloading install-service-from-web.sh file)..."
 
-wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/install-service-from-web.sh | bash -s $BRANCH || (echo "Failed to install plug and play service. Script: install-service-from-web.sh" && exit 1)
+wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/install-service-from-web.sh | bash -s - $BRANCH || (echo "Failed to install plug and play service. Script: install-service-from-web.sh" && exit 1)
 
 echo "Finished setting up plug and play"
