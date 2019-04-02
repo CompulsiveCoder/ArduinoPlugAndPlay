9cd054015d73377ca3d91173542e4dac49397d4c
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-02 02:29:23 +0000

    Added email error support to script

--- scripts-web/install-from-web.sh
+++ scripts-web/install-from-web.sh
@@ -31,7 +31,7 @@ cd $INSTALL_DIR
 
 echo "Initializing plug and play (by downloading init-from-web.sh file)..."
 
-wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/init-from-web.sh | bash -s - $BRANCH || (echo "Failed to initialize plug and play. Script: init-from-web.sh" && exit 1)
+wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/init-from-web.sh | bash -s - $BRANCH $SMTP_SERVER $ADMIN_EMAIL || (echo "Failed to initialize plug and play. Script: init-from-web.sh" && exit 1)
 
 echo "Initializing plug and play (by downloading install-service-from-web.sh file)..."
 
