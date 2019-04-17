d53c3e989f40c6a3f39222e04476c7c3c1d1e709
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-18 09:26:06 +1000

    Added connect timeouts to curl commands

--- scripts-web/start-from-web.sh
+++ scripts-web/start-from-web.sh
@@ -8,7 +8,7 @@ fi
 echo "Reinitializing plug and play (by downloading init-from-web.sh file)..."
 
 echo "Downloading init-from-web.sh script..."
-curl -o init-from-web.sh -f https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/init-from-web.sh || echo "Failed to download init-from-web.sh script"
+curl --connect-timeout 3 -o init-from-web.sh -f https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/init-from-web.sh || echo "Failed to download init-from-web.sh script"
 
 echo "Running init-from-web.sh script..."
 bash init-from-web.sh $BRANCH || exit 1
