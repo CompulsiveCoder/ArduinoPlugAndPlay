dc38f772fc7f32ce576d30ec669e4c956e8272e8
CompulsiveCoder <compulsivecoder@gmail.com>
2019-05-17 10:16:14 +1000

    Fixed install mono script to work on travis ci

--- install-mono.sh
+++ install-mono.sh
@@ -19,7 +19,7 @@ else
     $SUDO apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
     echo "deb http://download.mono-project.com/repo/ubuntu stable-$VERSION_NAME main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
   
-    $SUDO apt-get update -qq && $SUDO apt-get install -qq -y mono-devel mono-complete ca-certificates-mono msbuild
+    $SUDO apt-get update -qq && $SUDO apt-get install -qq -y --allow-unauthenticated mono-devel mono-complete ca-certificates-mono msbuild
   else
     echo "Mono is already installed. Skipping install."
   fi
