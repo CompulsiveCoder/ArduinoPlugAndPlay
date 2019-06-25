62f2af00933e869e55f4f64c485aa02512a5a857
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-26 07:49:32 +1000

    Trying to fix test

--- src/ArduinoPlugAndPlay.Tests.Integration/DeviceManagerAddDeviceRetryAfterErrorTestFixture.cs
+++ src/ArduinoPlugAndPlay.Tests.Integration/DeviceManagerAddDeviceRetryAfterErrorTestFixture.cs
@@ -155,7 +155,7 @@ rm ""devices/device1.txt""
             deviceManager.RunLoop ();
 
             Console.WriteLine ("Sleeping to let the remove process finish...");
-            Thread.Sleep (1000);
+            Thread.Sleep (2000);
 
             Console.WriteLine ("Checking that device1.txt was removed...");
             Console.WriteLine ("  " + device1FilePath);
