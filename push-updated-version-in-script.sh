c9f0b9c5b78834d3944eca3fca15aa51ba523c12
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-28 16:25:39 +1000

    Tweaked script to avoid CI failures

--- push-updated-version-in-script.sh
+++ push-updated-version-in-script.sh
@@ -5,7 +5,7 @@ BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ "$BRANCH" = "dev" ];  then
   git pull origin $BRANCH --quiet && \
   git commit scripts-installation/init.sh -m "Updated version in scripts-installation/init.sh script [ci skip]" && \
-  git push origin $BRANCH --quiet
+  git push origin $BRANCH --quiet || "Failed to push updated version in init.sh script"
 else
   echo "Skipping push updated version in script. Only pushed for 'dev' branch not '$BRANCH'"
 fi
