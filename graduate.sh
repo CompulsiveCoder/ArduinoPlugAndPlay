dcaba804247383251b8fd8a143b0f609910c24da
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-29 09:55:26 +1000

    Trying to fix graduate script

--- graduate.sh
+++ graduate.sh
@@ -5,25 +5,25 @@ BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ "$BRANCH" = "dev" ];  then
   echo "Graduating dev branch to master branch"
 
-  echo "  Fetching from origin"
+  echo "  Fetching from origin..."
   git fetch origin || exit 1
 
-  echo "  Merging master branch into dev branch"
-  git merge -X ours origin/master || exit 1
+  echo "  Merging master branch into dev branch..."
+  git pull origin master || exit 1
 
-  echo "  Checking out master branch"
+  echo "  Checking out master branch..."
   git checkout master || exit 1
 
   # Ensure it's up to date
   #git pull origin master --quiet && \
 
-  echo "  Merging dev branch into master branch"
+  echo "  Merging dev branch into master branch..."
   git merge -X theirs dev || exit 1
 
-  echo "  Pushing updates to master branch"
+  echo "  Pushing updates to master branch..."
   git push origin master || exit 1
 
-  echo "  Checkout out dev branch"
+  echo "  Checkout out dev branch..."
   git checkout dev || exit 1
 
   echo "The 'dev' branch has been graduated to the 'master' branch"  || exit 1
