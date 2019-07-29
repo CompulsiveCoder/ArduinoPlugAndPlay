d4c6e2fcfe3859de1fad774ee71b202f1871c62b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-29 12:47:52 +1000

    Fixed push version script

--- push-version.sh
+++ push-version.sh
@@ -2,10 +2,10 @@
 
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-if [ "$BRANCH" = "dev" ];  then
+#if [ "$BRANCH" = "dev" ];  then
   git pull origin $BRANCH --quiet && \
   git commit buildnumber.txt -m "Updated version [ci skip]" && \
   git push origin $BRANCH --quiet
-else
-  echo "Skipping push version. Only pushed for 'dev' branch not '$BRANCH'"
-fi
+#else
+#  echo "Skipping push version. Only pushed for 'dev' branch not '$BRANCH'"
+#fi
