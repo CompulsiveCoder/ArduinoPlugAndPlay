1e4dc97038c78bf565125de817a00ee714cfb467
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-15 12:58:13 +1100

    Updated duinocom version

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -4,6 +4,6 @@ echo "  Dir: $PWD"
 bash install-package-from-libs-repository.sh GreenSense NUnit 2.6.4 || exit 1
 bash install-package-from-libs-repository.sh GreenSense NUnit.Runners 2.6.4 || exit 1
 
-bash install-package-from-github-release.sh CompulsiveCoder duinocom.core 1.2.0.25 || exit 1
+bash install-package-from-github-release.sh CompulsiveCoder duinocom.core 1.2.0.27 || exit 1
 
 echo "Finished getting library files."
