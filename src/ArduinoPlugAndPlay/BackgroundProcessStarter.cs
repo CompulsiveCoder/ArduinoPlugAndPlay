217848b0f420be516a1f1691c3c1374311e7bf32
CompulsiveCoder <compulsivecoder@gmail.com>
2020-01-28 10:14:08 +1100

    Improved console output

--- src/ArduinoPlugAndPlay/BackgroundProcessStarter.cs
+++ src/ArduinoPlugAndPlay/BackgroundProcessStarter.cs
@@ -91,7 +91,7 @@ namespace ArduinoPlugAndPlay
                 // If the latest process isn't started then start it
                 if (processWrapper != null && !processWrapper.HasStarted) {
                     try {
-                        Console.WriteLine ("  Starting the next process in the queue: " + processWrapper.Action + " " + processWrapper.Info.GroupName);
+                        Console.WriteLine ("  Starting the next process in the queue: " + processWrapper.Action + " " + processWrapper.Info.ProjectName + " " + processWrapper.Info.DeviceName);
                         processWrapper.Start ();
                     } catch (Exception ex) {
                         IsError = true;
