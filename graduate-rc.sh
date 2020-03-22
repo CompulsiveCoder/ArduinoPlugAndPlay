67125f360000b486d3e8f03b719f164438bb7d3f
CompulsiveCoder <compulsivecoder@gmail.com>
2020-03-22 16:40:15 +1100

    Trying to fix graduate script

--- graduate-rc.sh
+++ graduate-rc.sh
@@ -6,23 +6,22 @@ echo "Graduating master branch to rc branch..."
 
 sh clean.sh || exit 1
 
-echo ""
-echo "Fetching from origin..."
-git fetch origin || exit 1
-
-
 if [ "$BRANCH" = "dev" ];  then
   echo "Currently in dev branch. Checking out master branch..."
   git checkout master || exit 1
 fi
 
-#echo ""
-#echo "Pulling the master branch from origin (to update it locally)..."
-#git pull origin master || exit 1
+echo ""
+echo "Fetching from origin..."
+git fetch origin || exit 1
+
+echo ""
+echo "Pulling the master branch from origin (to update it locally)..."
+git pull origin master || exit 1
 
 echo ""
 echo "Merging the rc branch into the master branch..."
-git merge -X ours origin/rc || exit 1
+git merge rc || exit 1
 
 echo ""
 echo "Checking out the rc branch..."
@@ -34,15 +33,23 @@ git checkout rc || exit 1
 
 echo ""
 echo "Merging the master branch into the rc branch..."
-git merge -X theirs origin/master || exit 1
+git merge -X theirs master || exit 1
 
 echo ""
 echo "Pushing the updated rc branch to origin..."
 git push origin rc || exit 1
 
 echo ""
+echo "Fomastering remote test..."
+sh force-remote-test.sh || exit 1
+
+echo ""
 echo "Checking out the $BRANCH branch again..."
 git checkout $BRANCH || exit 1
+
+echo ""
+echo "Merging rc into $BRANCH"
+git merge rc
 
 echo ""
 echo "The 'master' branch has been graduated to the 'rc' branch"
