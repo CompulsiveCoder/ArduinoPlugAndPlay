1e618eee2cf2d8370e8ec8e2001a764d0364fc6d
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-15 17:01:13 +1100

    Removing sudo in script. If sudo is required the script can be called with sudo.

--- systemctl.sh
+++ systemctl.sh
@@ -4,9 +4,10 @@ MOCK_FLAG_FILE="is-mock-systemctl.txt"
 
 if [ ! -f $MOCK_FLAG_FILE ]; then
   SUDO=""
-  if [ ! "$(id -u)" -eq 0 ]; then
-      SUDO='sudo'
-  fi
+  # TODO: Clean up. Sudo is disabled. If sudo is required the script can be called with sudo
+  #if [ ! "$(id -u)" -eq 0 ]; then
+  #    SUDO='sudo'
+  #fi
   $SUDO systemctl $1 $2 $3
 else
   echo "[mock] sudo systemctl $1 $2 $3"
